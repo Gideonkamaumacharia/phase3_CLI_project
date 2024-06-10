1ad978dffb16e127bcbde1bfa2a69e02ecbf7858
Gideon Macharia <kamaugedion95@gmail.com>
2024-06-10 16:02:02 +0300

    create view_tasks

--- tasks.py
+++ tasks.py
@@ -36,6 +36,28 @@ class TasksDb:
         conn.commit()
         print("Task marked as completed.")
 
+    def view_tasks(self):
+        c.execute('''SELECT * FROM tasks WHERE completed = 0 ORDER BY deadline ASC''')
+        tasks = c.fetchall()
+        if not tasks:
+            print("No pending tasks.")
+        else:
+            for task in tasks:
+                print(task)
+
+    def add_category(self,name):
+        c.execute('''INSERT INTO categories (name) VALUES (?)''', (name,))
+        conn.commit()
+        print("Category added successfully.")
+
+    def view_categories(self):
+        c.execute('''SELECT * FROM categories''')
+        categories = c.fetchall()
+        if not categories:
+            print("No categories found.")
+        else:
+            for category in categories:
+                print(category)
     
 def main():
     
@@ -45,6 +67,11 @@ def main():
         print("\nTask Manager CLI\n")
         print("1.Add Task")
         print("2.Mark Task As Complete")
+        print("3. View Tasks")
+        print("4. Add Category")
+        print("5. View Categories")
+        print("6. Exit")
+
 
         choice  = input("\nEnter your choice: ")
 
@@ -58,6 +85,22 @@ def main():
         elif choice == '2':
             task_id = input("Enter task ID to mark as completed: ")
             db.mark_task_completed(task_id)
+
+        elif choice == '3':
+            db.view_tasks()
+
+        elif choice == '4':
+            name = input("Enter category name: ")
+            db.add_category(name)
+
+        elif choice == '5':
+            db.view_categories()
+
+        elif choice == '6':
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
 
             
 
