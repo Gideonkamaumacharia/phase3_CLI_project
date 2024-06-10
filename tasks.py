f9be8787af5cae02857d2808aaa600acab26a9ca
Gideon Macharia <kamaugedion95@gmail.com>
2024-06-10 13:13:09 +0300

    create tables

--- tasks.py
+++ tasks.py
@@ -1,5 +1,13 @@
 from __init__ import conn,c
 
-
+def create_table():
+    c.execute("""CREATE TABLE IF NOT EXISTS tasks(
+              id INTEGER PRIMARY KEY,
+              title TEXT NOT NULL,
+              description TEXT NOT NULL,
+              deadline DATE NOT NULL,
+              category TEXT NOT NULL,
+              completed INTEGER DEFAULT 0
+              )""")
 
    
