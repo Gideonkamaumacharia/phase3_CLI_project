199be9798757cba06d492c4a90ee28fafb822788
Gideon Macharia <kamaugedion95@gmail.com>
2024-06-10 13:29:27 +0300

    initialize connection

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 from __init__ import conn,c
+import sqlite3
 
 def create_table():
     c.execute("""CREATE TABLE IF NOT EXISTS tasks(
@@ -9,5 +10,24 @@ def create_table():
               category TEXT NOT NULL,
               completed INTEGER DEFAULT 0
               )""")
+    c.execute("""CREATE TABLE IF NOT EXISTS categories(
+               id INTEGER PRIMARY KEY,
+               name TEXT NOT NULL
+              )""")
+    conn.commit()
+    conn.close()
+
+create_table()
+
+class Tasks:
+    def __init__(self):
+        self.conn = sqlite3.connect("tasks_manager.db")
+        self.c = self.conn.cursor()
+
+    
+        
+
+
+
 
    
